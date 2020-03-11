868ec9ee5439c55c474ccd3ccab9328d899248ef
pickles7 <32805499+pickles7@users.noreply.github.com>
2020-03-10 20:42:57 -0500

    Update server.py

--- app/server.py
+++ app/server.py
@@ -62,7 +62,7 @@ async def analyze(request):
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
     probs = learn.predict(img)[2]
-    return JSONResponse({'classification': str(prediction)+"boo"})
+    return JSONResponse({'classification': str(prediction)})
     #  return JSONResponse({'result': str(prediction)}, {'prob0': probs[0]},{'prob1': probs[1]})
 
 
